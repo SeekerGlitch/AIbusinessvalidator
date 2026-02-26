ecb099480460fffd403eac8716dd4a6cbc7255a0
SeekerGlitch <arshia82work@gmail.com>
2026-02-26 09:35:48 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,79 @@
+#input --> business idea
+#country
+#audience
+#online, offline, product
+
+#streamlit, google-genai, reportlab
+#AIzaSyCVFhX6EaTpyl8KO74XfeXxFEbb0bVdd9g
+
+import streamlit as st
+from google import genai
+from google.genai import types
+import os
+from reportlab.pdfgen import canvas
+
+st.title("AI business Idea Validator")
+
+idea = st.text_area("Business Idea")
+location =st.text_input("Target Country & city")
+biz_type =st.selectbox ("Business type", ["online", "offline", "product", "service"])
+audience = st.text_input("Whose your audience")
+
+client = genai.Client(api_key ="AIzaSyCVFhX6EaTpyl8KO74XfeXxFEbb0bVdd9g")
+
+if st.button("Generate report"):
+
+    prompt = f"""
+
+    Analyse this business idea:
+    Idea : {idea}
+    Location: {location}
+    type: {biz_type}
+    audience: {audience}
+
+
+    Give a structured report with:
+    1.Overview
+    2.Market Analysis
+    3.Revenue possibility
+    4.Funding options
+    5.Competitor Analysis
+
+
+
+"""
+    answer = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents = prompt
+        )
+    st.subheader("AI Analysis")
+    st.write(answer.text)
+
+
+    report_content = answer.text
+
+    pdffile = "Analysis_report.pdf"
+
+    c = canvas.Canvas(pdffile)
+    c.setFont("Helvetica",12)
+
+    y = 800
+    #\n --> new line
+    for line in report_content.split("\n"):
+        c.drawString(50,y,line)
+        y= y - 15
+
+        if y<50:
+            c.showPage()
+            y=800
+    c.save()
+
+    with open(pdffile,"rb") as file:
+              st.download_button(
+                  label="Download report",
+                  data =file,
+                  file_name = "Analysis_report.pdf"
+
+                  )
+    
+    
